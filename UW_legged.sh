eeb23b273e79a64be965fdec090da1961b54cc1a
Rohan Panicker <rohanpanicker@Rohans-MacBook-Pro.local>
2023-11-08 16:05:42 -0800

    Added bash scripts

---
+++ UW_legged.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#clone this repo outside the docker 
+git clone https://github.com/UWRobotLearning/unitree_legged_control.git
---
+++ UW_legged_requirements.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#install requirements inside the docker
+apt-get update
+apt install liblcm-dev
